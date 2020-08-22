9dfda8075531e1a9c8f3ee32e3cf8e0d40a077cb
cbohan3 <connor.bohan@gmail.com>
2020-08-21 19:39:59 -0700

    Initial UI

---
+++ justbreathe/Assets/KeyDisplay/KeyDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class KeyDisplay : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI keyText;
+    [SerializeField] private TextMeshProUGUI keyStateText;
+    [SerializeField] private TextMeshProUGUI keyTargetStateText;
+    [SerializeField] private RectTransform fillTransform;
+    [SerializeField] private float maxFillWidth;
+
+    [Header("Vars")]
+    public string Key;
+    public string KeyState;
+    public string TargetState;
+    public float FillAmount;
+
+    private void Update()
+    {
+        keyText.text = Key;
+        keyStateText.text = KeyState;
+        keyTargetStateText.text = TargetState;
+
+        FillAmount = Mathf.Clamp01(FillAmount);
+        fillTransform.sizeDelta = new Vector2(maxFillWidth * FillAmount, fillTransform.sizeDelta.y);
+    }
+}
---
+++ justbreathe/Assets/KeyDisplay/KeyDisplayContainer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class KeyDisplayContainer : MonoBehaviour
+{
+    private static KeyDisplayContainer instance;
+
+    [SerializeField] private KeyDisplay keyDisplayPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+
+        CreateKeyDisplay("E");
+        CreateKeyDisplay("F");
+    }
+
+    public static KeyDisplay CreateKeyDisplay(string keyName)
+    {
+        KeyDisplay keyDisplay = Instantiate(instance.keyDisplayPrefab, instance.transform);
+        keyDisplay.Key = keyName;
+
+        return keyDisplay;
+    }
+}
