4dd2cf271cfe3f6dcaa3c5dbfbf2e679b4fe2673
cbohan3 <connor.bohan@gmail.com>
2020-08-22 10:13:52 -0700

    Lines are now heckin smooth

--- justbreathe/Assets/KeyDisplay/UILineRenderer.cs
+++ justbreathe/Assets/KeyDisplay/UILineRenderer.cs
@@ -21,10 +21,23 @@ public class UILineRenderer : Graphic
         UIVertex vertex = UIVertex.simpleVert;
         vertex.color = color;
 
+        float curAngle = 0;
+        float angle = 0;
+        int numVertexGroups = 0;
         for (int i = 0; i < pointPositions.Length; i++)
-            DrawVerticesForPoint(pointPositions[i], vh);
+        {
+            if (i < pointPositions.Length - 1)
+                angle = GetAngle(pointPositions[i], pointPositions[i + 1]);
 
-        for (int i = 0; i < pointPositions.Length - 1; i++)
+            do 
+            {
+                numVertexGroups++;
+                DrawVerticesForPoint(pointPositions[i], vh, curAngle);
+                curAngle = Mathf.MoveTowards(curAngle, angle, 5f);
+            } while (Mathf.Abs(curAngle - angle) > 3f);
+        }
+
+        for (int i = 0; i < numVertexGroups - 1; i++)
         {
             int index = i * 2;
             vh.AddTriangle(index + 0, index + 1, index + 3);
@@ -32,22 +45,30 @@ public class UILineRenderer : Graphic
         }
     }
 
-    private void DrawVerticesForPoint(Vector2 point, VertexHelper vh)
+    private void DrawVerticesForPoint(Vector2 point, VertexHelper vh, float angle)
     {
         UIVertex vertex = UIVertex.simpleVert;
         vertex.color = color;
 
-        vertex.position = GetPosition(point.x, point.y, 0, -Thickness / 2f);
+        vertex.position = GetPosition(point.x, point.y, 0, -Thickness / 2f, angle);
         vh.AddVert(vertex);
 
-        vertex.position = GetPosition(point.x, point.y, 0, Thickness / 2f);
+        vertex.position = GetPosition(point.x, point.y, 0, Thickness / 2f, angle);
         vh.AddVert(vertex);
     }
 
-    private Vector3 GetPosition(float widthOffset, float heightOffset, float xOffset = 0, float yOffset = 0)
+    private float GetAngle(Vector2 me, Vector2 next)
     {
-        return new Vector3(
-            (rectTransform.rect.width * widthOffset) - (rectTransform.rect.width / 2f) + xOffset,
-            (rectTransform.rect.height * heightOffset) - (rectTransform.rect.height / 2f) + yOffset);
+        return (float)(Mathf.Atan2(next.y - me.y, next.x - me.x) * (180f / Mathf.PI));
+    }
+
+    private Vector3 GetPosition(float widthOffset, float heightOffset, float xOffset = 0, float yOffset = 0, float angle = 0)
+    {
+        Vector3 position = Quaternion.Euler(0, 0, angle) * new Vector3(xOffset, yOffset);
+        position += new Vector3(
+            (rectTransform.rect.width * widthOffset) - (rectTransform.rect.width / 2f),
+            (rectTransform.rect.height * heightOffset) - (rectTransform.rect.height / 2f));
+
+        return position;
     }
 }
