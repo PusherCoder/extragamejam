eab69734508b12544356ba1cf0805671b62267a9
PusherCoder <bendegn@gmail.com>
2020-08-22 18:25:31 -0700

    Programming First Scenario

--- justbreathe/Assets/GameManager.cs
+++ justbreathe/Assets/GameManager.cs
@@ -39,15 +39,36 @@ public class GameManager : MonoBehaviour
         BreathKey.OnFail.AddListener(Asphyxiate);
         HeartKey.OnFail.AddListener(HeartAttack);
 
-        deathScreenCanvasGroup.alpha = 0;
         ScenarioTime = 0.0f;
         ScenarioPosition = 0;
 
         Scenario1 = new GameScriptController[]
         {
-            new GameScriptController{ ScenarioTime = 10.0f, Subtitle = "", Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 1.0f, TimeUp = 1.0f } } },
-            new GameScriptController{ ScenarioTime = 100.0f, Subtitle = "Feeling Dizzy", Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 0.25f, TimeUp = 0.25f } } }
+            new GameScriptController{ ScenarioTime = 0.0f,    Subtitle = "",
+                                      Adjustment = null },
+            new GameScriptController{ ScenarioTime = 2.0f,    Subtitle = "Looks like cereal again.",
+                                      Adjustment = null },
+            new GameScriptController{ ScenarioTime = 10.0f,   Subtitle = "*crunching sounds*",
+                                      Adjustment = null },
+            new GameScriptController{ ScenarioTime = 18.0f,   Subtitle = "I wonder if all this sugar is good for me?",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = HeartKey, TimeDown = 0.9f, TimeUp = 1.0f } } },
+            new GameScriptController{ ScenarioTime = 25.0f,   Subtitle = "*crunching sounds*",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = HeartKey, TimeDown = 0.8f, TimeUp = 1.0f } } },
+            new GameScriptController{ ScenarioTime = 35.0f,   Subtitle = "*drinking sounds*",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = HeartKey, TimeDown = 1.0f, TimeUp = 1.0f } } },
+            new GameScriptController{ ScenarioTime = 45.0f,   Subtitle = "Wait, did I forget to pay the power bill again?",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 1.5f, TimeUp = 1.5f } } },
+            new GameScriptController{ ScenarioTime = 50.0f,   Subtitle = "*papers shuffling*",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 1.2f, TimeUp = 1.5f } } },
+            new GameScriptController{ ScenarioTime = 60.0f,   Subtitle = "*Whew* I must have forgotten that I paid it early.",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 1.8f, TimeUp = 1.8f } } },
+            new GameScriptController{ ScenarioTime = 70.0f,   Subtitle = "I wish today was Saturday.",
+                                      Adjustment = new RhythmAdjustment[]{ new RhythmAdjustment{ Key = BreathKey, TimeDown = 2.0f, TimeUp = 2.0f } } },
+            new GameScriptController{ ScenarioTime = 80.0f,   Subtitle = "*crunching sounds*",
+                                      Adjustment = null }
         };
+
+        deathScreenCanvasGroup.alpha = 0;
     }
     
     private void Asphyxiate()
@@ -74,10 +95,9 @@ public class GameManager : MonoBehaviour
         if( ScenarioTime > Scenario1[ScenarioPosition].ScenarioTime )
         {
             ScenarioPosition++;
-            ScenarioTime = Scenario1[ScenarioPosition].ScenarioTime;
-            for( int i = 0; i < Scenario1[ScenarioPosition].Adjustment.Length; i++ )
+            Subtitles.text = Scenario1[ScenarioPosition].Subtitle;
+            for ( int i = 0; i < Scenario1[ScenarioPosition].Adjustment.Length; i++ )
             {
-                Subtitles.text = Scenario1[ScenarioPosition].Subtitle;
                 Scenario1[ScenarioPosition].Adjustment[i].Key.TimeDown = Scenario1[ScenarioPosition].Adjustment[i].TimeDown;
                 Scenario1[ScenarioPosition].Adjustment[i].Key.TimeUp = Scenario1[ScenarioPosition].Adjustment[i].TimeUp;
             }
